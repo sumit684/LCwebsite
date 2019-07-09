43a851ee83c2ffdc574d2d96137cbe9b21dd60c6
sumit684 <singh.sumit684@gmail.com>
2019-07-10 01:03:06 +0530

    Update rdso_samp.php

--- rdso_samp.php
+++ rdso_samp.php
@@ -12,7 +12,7 @@ include('session.php');
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <style type="text/css">
+ <style type="text/css">
   	/*body{
   		margin: 50px;
   	}*/
